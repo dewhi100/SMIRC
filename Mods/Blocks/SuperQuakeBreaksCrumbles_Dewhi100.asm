4c7521a771ebbf5595aad9408edb193286ced827
dewhi100 <mrseandewhirst@gmail.com>
2026-07-11 15:18:41 -0400

    Housekeeping

--- Mods/Blocks/SuperQuakeBreaksCrumbles_Dewhi100.asm
+++ Mods/Blocks/SuperQuakeBreaksCrumbles_Dewhi100.asm
@@ -1,10 +1,13 @@
 lorom
 
 ;manualy break crumbles in a room when supers impact anything. uses room ASM and hardcoded coords
+;will need different copies of this for each room you want to have the effect.
 
-org $8FE99B
+org !free8F
 LDA $0C18 :  BIT #$0800 : BEQ +
 JSL $8483D7
 DB $1D : DB $03 : DW $d054
 +
 RTS
+
+!free8F#= pc()
