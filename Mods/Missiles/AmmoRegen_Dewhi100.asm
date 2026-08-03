39df911a1a667060e4be87dc02f779980113d210
dewhi100 <mrseandewhirst@gmail.com>
2026-08-02 23:34:42 -0400

    Passive Missile Regeneration - dewhi100

---
+++ Mods/Missiles/AmmoRegen_Dewhi100.asm
@@ -0,0 +1,47 @@
+;Missile regeneration by dewhi100
+;Your missiles regenerate over time. 
+;Regeneration speed is on an exponential curve based on your max missiles.
+;With only 5 max missiles, regeneration is painfully slow. something like 1 missile every 45 seconds.
+;With 255 missiles, you're getting a missile every couple *frames*.
+
+LoROM
+
+org $82DBB2	;every frame
+JSR RegenAmmo
+
+org !free82
+RegenAmmo:
+LDA !AmmoRegenSubammo
+ADC	!AmmoRegenRate_SRAM
+BPL +
+AND $7FFF
+STA !AmmoRegenSubammo
+LDA !missilesCurrent
+CMP !missilesMax
+BEQ ++
+INC !missilesCurrent
+++
+RTS
++
+STA !AmmoRegenSubammo
+RTS
+
+!free82 #= pc()
+
+org $8489A9	;on ammo collect:
+JSR SetAmmoRegenRate
+
+org !free84
+SetAmmoRegenRate:
+LDA !missilesMax
+XBA
+ORA !missilesMax
+STA $4202	;writes 8-bit max HP to multiplication registers
+NOP #4		;wait for result
+LDA $4216
+LSR #1
+STA !AmmoRegenRate_SRAM
+LDA !missilesCurrent	;hijacked instruction
+RTS
+
+!free84 #= pc()
